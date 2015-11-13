2fcf2c11448a0949d2109d53a61acff025609196
Andrew Hurst <andrew@hurstdog.org>
2015-11-13 15:01:04 -0800

    dup3 initial commit

---
+++ ch1/dup3.go
@@ -0,0 +1,28 @@
+// Dup3 is just like dup2, but slups the whole file into memory in one go.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
