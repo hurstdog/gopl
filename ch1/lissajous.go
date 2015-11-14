a03ca09618d9d150a8238f63e9250455a9543725
Andrew Hurst <andrew@hurstdog.org>
2015-11-13 16:45:31 -0800

    Added color rotations

--- ch1/lissajous.go
+++ ch1/lissajous.go
@@ -11,12 +11,19 @@ import (
 	"os"
 )
 
-var palette = []color.Color{color.Black, color.RGBA{0x0, 0x80, 0x0, 0xff}}
+var palette = []color.Color{
+	color.Black,
+	color.RGBA{0xff, 0x00, 0x0, 0xff}, // red
+	color.RGBA{0x0, 0xff, 0x0, 0xff},  // green
+	color.RGBA{0x0, 0x0, 0x80, 0xff},  // blue
+}
 
 const (
 	backgroundIndex = 0 // first color in palette
-	foregroundIndex = 1 // next color in palette
 )
+
+const minColorIndex = 1
+const maxColorIndex = 3
 
 func main() {
 	lissajous(os.Stdout)
@@ -33,6 +40,9 @@ func lissajous(out io.Writer) {
 	freq := rand.Float64() * 3.0 // relative frequncy of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
+	colorCount := 0
+	// Change the color every 10 frames
+	colorIndex := minColorIndex
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
@@ -40,7 +50,15 @@ func lissajous(out io.Writer) {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				foregroundIndex)
+				uint8(colorIndex))
+		}
+		colorCount++
+		if colorCount >= 10 {
+			colorCount = 0
+			colorIndex++
+			if colorIndex > maxColorIndex {
+				colorIndex = minColorIndex
+			}
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
