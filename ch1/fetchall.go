7353878eb656a409e9238189f4027b81f82dd2db
Andrew Hurst <andrew@hurstdog.org>
2015-11-14 21:49:56 -0800

    Write all the outputs to a file.

--- ch1/fetchall.go
+++ ch1/fetchall.go
@@ -3,10 +3,10 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -27,16 +27,27 @@ func main() {
 func fetch(url string, ch chan<- string) {
 	start := time.Now()
 	resp, err := http.Get(url)
+	defer resp.Body.Close() // don't leak resources
 	if err != nil {
 		ch <- fmt.Sprint(err)
 		return
 	}
-	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
-	resp.Body.Close() // don't leak resources
+	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		ch <- fmt.Sprintf("while reading %s: %v", url, err)
 		return
 	}
+	fn := urlToFilename(url)
+	err = ioutil.WriteFile(fn, b, 0644)
+	if err != nil {
+		ch <- fmt.Sprintf("while writing %s: %v", fn, err)
+		return
+	}
 	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs\t%7d\t%s", secs, nbytes, url)
+	ch <- fmt.Sprintf("%.2fs\t%7d\t%s", secs, len(b), url)
+}
+
+// Returns a url with all slashes replaces with _
+func urlToFilename(url string) string {
+	return strings.Replace(url, "/", "_", -1)
 }
