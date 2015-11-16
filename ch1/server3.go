50c536f799b786f30f5bbde214649df2fb9e8441
Andrew Hurst <andrew@hurstdog.org>
2015-11-15 21:04:16 -0800

    Added lissajous

--- ch1/server3.go
+++ ch1/server3.go
@@ -3,7 +3,13 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
 	"sync"
 )
@@ -11,9 +17,26 @@ import (
 var mu sync.Mutex
 var count int
 
+var palette = []color.Color{
+	color.Black,
+	color.RGBA{0xff, 0x00, 0x0, 0xff}, // red
+	color.RGBA{0x0, 0xff, 0x0, 0xff},  // green
+	color.RGBA{0x0, 0x0, 0x80, 0xff},  // blue
+}
+
+const (
+	backgroundIndex = 0 // first color in palette
+)
+
+const minColorIndex = 1
+const maxColorIndex = 3
+
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/count", counter)
+	http.HandleFunc("/lissajous", func(w http.ResponseWriter, r *http.Request) {
+		lissajous(w)
+	})
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
@@ -44,4 +67,42 @@ func counter(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	fmt.Fprintf(w, "Count = %d\n", count)
 	mu.Unlock()
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequncy of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	colorCount := 0
+	// Change the color every 10 frames
+	colorIndex := minColorIndex
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				uint8(colorIndex))
+		}
+		colorCount++
+		if colorCount >= 10 {
+			colorCount = 0
+			colorIndex++
+			if colorIndex > maxColorIndex {
+				colorIndex = minColorIndex
+			}
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: Ignoring encoding errors
 }
