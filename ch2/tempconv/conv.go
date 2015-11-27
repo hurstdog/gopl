62cdb3f1da1693afa0e22c2096ddbbc8294cc36f
Andrew Hurst <andrew@hurstdog.org>
2015-11-27 12:53:46 -0800

    Added support for Kelvin

--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -5,3 +5,15 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
 // FToC converts a Fahrenheit temperature to Celsius.
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+// CToK converts a Celcius temperature to Kelvin.
+func CToK(c Celsius) Kelvin { return Kelvin(c - 273.15) }
+
+// FToK converts a Fahrenheit temperature to Kelvin.
+func FToK(f Fahrenheit) Kelvin { return CToK(FToC(f)) }
+
+// Kelvin -> Celsius
+func KToC(k Kelvin) Celsius { return k + 273.15 }
+
+// Kelvin -> Fahrenheit
+func KToF(k Kelvin) Fahrenheit { return CToF(KToC(k)) }
--- ch2/tempconv/tempconv.go
+++ ch2/tempconv/tempconv.go
@@ -4,6 +4,7 @@ package tempconv
 import "fmt"
 
 type Celsius float64
+type Kelvin float64
 type Fahrenheit float64
 
 const (
@@ -13,4 +14,5 @@ const (
 )
 
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
-func (c Fahrenheit) String() string { return fmt.Sprintf("%g°F", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func (k Kelvin) String() string     { return fmt.Sprintf("%g°K", k) }
