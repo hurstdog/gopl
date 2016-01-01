a8515a85329eb781c8db1bc03d4c015c5d315a5d
Andrew Hurst <andrew@hurstdog.org>
2016-01-01 15:40:28 -0800

    Added support for floating point numbers

--- ch3/comma.go
+++ ch3/comma.go
@@ -4,6 +4,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,6 +15,10 @@ func main() {
 
 // recursion!
 func comma(s string) string {
+	// If there's a dot in the number, only comma'-ize the part before the dot.
+	if dot := strings.Index(s, "."); dot >= 0 {
+		return comma(s[:dot]) + s[dot:]
+	}
 	n := len(s)
 	if n <= 3 {
 		return s
